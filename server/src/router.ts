bdcee24d59d5742635a44d1591488279f60e1899
Luigi-44 <luigi.rondeau@gmail.com>
2025-02-10 15:26:19 +0100

    Etape 03 JSmonorepo

--- server/src/router.ts
+++ server/src/router.ts
@@ -15,4 +15,18 @@ router.post("/api/items", itemActions.add);
 
 /* ************************************************************************* */
 
+// Declaration of a "Welcome" route
+
+import sayActions from "./modules/say/sayActions";
+
+router.get("/", sayActions.sayWelcome);
+
+/* ************************************************************************* */
+
+//import de programAction
+
+import programActions from "./modules/program/programActions";
+
+router.get("/api/programs", programActions.browse);
+
 export default router;
